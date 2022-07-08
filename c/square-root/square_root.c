40db5bce44d976fdfefd787b792648f7b57eba62
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-07-09 00:15:56 +0900

    adding a solution

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,1 +1,22 @@
 #include "square_root.h"
+
+int square_root(int radicand)
+{
+    /* Binary numeral system (base 2) */
+    int x = radicand;
+    int c = 0;
+    int d = 1 << 30;
+    while (d > radicand)
+        d >>= 2;
+
+    while (d != 0) {
+        if (x >= c + d) {
+            x -= c + d;
+            c = (c >> 1) + d;
+        } else {
+            c >>= 1;
+        }
+        d >>= 2;
+    }
+    return c;
+}

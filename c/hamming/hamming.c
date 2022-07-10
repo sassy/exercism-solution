b3c8035312ce0dac3836a35ec61ec0eb5ebf32cb
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-07-10 13:06:32 +0900

    write to better code.

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -2,11 +2,11 @@
 
 int compute(const char *lhs, const char *rhs) {
     int ret = 0;
-    for (;*lhs != '\0' && *rhs != '\0' ; lhs++, rhs++) {
+    for (;*lhs && *rhs ; lhs++, rhs++) {
         if (*lhs != *rhs) {
             ret++;
         }
     }
 
-    return (*lhs != '\0' || *rhs != '\0') ? -1 : ret;
+    return (*lhs||*rhs) ? -1 : ret;
 }

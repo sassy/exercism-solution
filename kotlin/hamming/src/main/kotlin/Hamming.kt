3d9f90d85e0d8633f5a45ac6d6537e7542d05f37
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-07-10 13:44:16 +0900

    adding a solution

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,8 +1,7 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        if (leftStrand.length != rightStrand.length) 
-            throw IllegalArgumentException("left and right strands must be of equal length")
+        require (leftStrand.length == rightStrand.length) {"left and right strands must be of equal length" }
     
         var length = 0;
         for ((index, char)  in leftStrand.withIndex()) {

076007b478ff0e6731d0b04c50123b8627b8c02d
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-07-12 00:24:19 +0900

    clean project

--- kotlin/matrix/src/main/kotlin/Matrix.kt
+++ kotlin/matrix/src/main/kotlin/Matrix.kt
@@ -1,15 +1,9 @@
 class Matrix(private val matrixAsString: String) {
     private val rows = matrixAsString.split('\n')
 
-    fun column(colNr: Int): List<Int> {
-        val columns: MutableList<Int> = mutableListOf()
-        for (row in rows) {
-            columns.add(row.trim().split("\\s+".toRegex())[colNr -1].toInt())
-        }
-        return columns.toList()
+    fun column(colNr: Int): List<Int> = rows.map {
+        it.trim().split("\\s+".toRegex())[colNr -1].toInt()
     }
 
-    fun row(rowNr: Int): List<Int> {
-        return rows[rowNr - 1].trim().split("\\s+".toRegex()).map{it.toInt()}
-    }
+    fun row(rowNr: Int): List<Int> = rows[rowNr - 1].trim().split("\\s+".toRegex()).map{it.toInt()}
 }

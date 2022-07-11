57ad6148231631744a233212811290d6d1aca57e
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-07-11 23:37:38 +0900

    adding a solution

---
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -0,0 +1,21 @@
+object ScrabbleScore {
+
+    fun scoreLetter(c: Char): Int = when(c.toUpperCase()) {
+        'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> 1
+        'D', 'G' -> 2
+        'B', 'C', 'M', 'P' -> 3
+        'F', 'H', 'V', 'W', 'Y' -> 4
+        'K' -> 5
+        'J', 'X' -> 8
+        'Q', 'Z' -> 10
+        else -> 0
+    }
+
+    fun scoreWord(word: String): Int {
+        var ret = 0;
+        for (char in word) {
+            ret += scoreLetter(char)
+        }
+        return ret
+    }
+}

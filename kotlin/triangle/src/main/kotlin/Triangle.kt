967105020ce626c2967f03318fdc96bd5ec8428a
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-07-12 23:12:12 +0900

    adding a solution

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,12 +1,8 @@
 class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
     init {
-        if (a.toDouble() + c.toDouble() < b.toDouble()) {
-            throw IllegalArgumentException()    
-        }
-        if (a.toDouble() + b.toDouble() < c.toDouble()) {
-            throw IllegalArgumentException()    
-        }
-        if (b.toDouble() + c.toDouble() < a.toDouble()) {
+        if (!((a.toDouble() + c.toDouble() > b.toDouble()) 
+            and (a.toDouble() + b.toDouble() > c.toDouble()) 
+            and (b.toDouble() + c.toDouble() > a.toDouble()))) {
             throw IllegalArgumentException()    
         }
         require(!((a == 0) and (b == 0) and (c ==0)))

57cff723076b96435a36b97fa35e06e9eb58d990
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-09-07 20:59:03 +0900

    update ISBN Verifier for Dart.

--- dart/isbn-verifier/lib/isbn_verifier.dart
+++ dart/isbn-verifier/lib/isbn_verifier.dart
@@ -1,12 +1,10 @@
 bool isValid(String isbn) {
+  if (!RegExp(r'[0-9-]{9,}[0-9|X]$').hasMatch(isbn)) return false;
   final codes = isbn.replaceAll('-', '').split('');
   if (codes.length != 10) return false;
 
   int cd = 0;
   for (int i = 0; i < codes.length; i++) {
-    if (codes[i] == 'X' && i != 9) {
-      return false;
-    } else if (new RegExp(r'[A-W|Y-Z]').hasMatch(codes[i])) return false;
     cd += int.parse(codes[i] == 'X' ? '10' : codes[i]) * (10 - i);
   }
   return (cd % 11) == 0;

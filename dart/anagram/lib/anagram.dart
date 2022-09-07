839912240e007f26e0803916a10f39d6c1ef3960
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-09-07 19:21:56 +0900

    update Anagram for Dart.

--- dart/anagram/lib/anagram.dart
+++ dart/anagram/lib/anagram.dart
@@ -1,20 +1,18 @@
 class Anagram {
   bool checkSpell(List<String> parts, String target) {
-    List<String> checkTargets = target.split('');
+    List<String> checkTargets = target.toLowerCase().split('');
     List<String> checkParts = List.from(parts);
     bool ret = true;
     for (int i = 0; i < target.length; i++) {
       final c = checkParts.removeLast();
-      ret = ret &&
-          (checkTargets.contains(c.toLowerCase()) ||
-              checkTargets.contains(c.toUpperCase()));
+      ret = ret && (checkTargets.contains(c));
       checkTargets.remove(c);
     }
     return ret;
   }
 
   List<String> findAnagrams(String given, List<String> candidates) {
-    var parts = given.split('');
+    var parts = given.toLowerCase().split('');
 
     List<String> result = candidates
         .where((element) =>

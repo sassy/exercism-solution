37f03626b6f783697e9e1731ff1af141d8830296
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-09-29 23:34:20 +0900

    [Scheme] update Leap solution.

--- scheme/leap/leap.scm
+++ scheme/leap/leap.scm
@@ -2,12 +2,10 @@
 
 (define (leap-year? year)
   (if 
-    (= (modulo year 4) 0)
-    (if
-      (or (= (modulo year 400) 0)  (not (= (modulo year 100) 0)))
-      #t
-      #f
-    )
+    (and (zero? (modulo year 4))
+      (or (zero? (modulo year 400))
+        (not (zero? (modulo year 100)))))
+    #t
     #f
   )
 )

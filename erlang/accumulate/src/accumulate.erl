4a5d572c66824c9ec003f396089c124a080d4637
Satoshi Watanabe <sassy.watanabe@gmail.com>
2023-08-03 14:30:05 +0900

    [Erlang] resolved Accumulate.

---
+++ erlang/accumulate/src/accumulate.erl
@@ -0,0 +1,9 @@
+-module(accumulate).
+-export([accumulate/2]).
+
+%%
+%% given a fun and a list, apply fun to each list item replacing list item with fun's return value.
+%%
+-spec accumulate(fun((A) -> B), list(A)) -> list(B).
+accumulate(_, []) -> [];
+accumulate(Fn, [H|T]) -> [Fn(H)|accumulate(Fn, T)].
